b0bccfd67bb42a37170d04d98613ca93af58aa55
ShadowCurse <shadowcurse57@gmail.com>
2022-02-26 16:47:13 +0300

    added objects to the dev level

--- src/game/dev.rs
+++ src/game/dev.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use heron::prelude::*;
 use iyes_bevy_util::BevyState;
 
 /// This plugin should add all Scenario1 specific stuff
@@ -9,6 +10,72 @@ pub struct DevPlaygroundPlugin<S: BevyState> {
 impl<S: BevyState> Plugin for DevPlaygroundPlugin<S> {
     fn build(&self, app: &mut App) {
         // add systems to `self.state`
+        app.add_system_set(SystemSet::on_enter(self.state.clone()).with_system(setup_scene));
     }
 }
 
+fn setup_scene(mut commands: Commands) {
+    // enemy
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(20.0, 20.0)),
+                color: Color::rgb(0.9, 0.2, 0.2),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)),
+            ..Default::default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(CollisionShape::Sphere { radius: 20.0 });
+
+    // walls
+    // top
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(500.0, 10.0)),
+                color: Color::rgb(0.9, 0.9, 0.9),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(100.0, 300.0, 0.0),
+            ..Default::default()
+        })
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(250.0, 5.0, 0.1),
+            border_radius: None,
+        });
+    // middle
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(30.0, 100.0)),
+                color: Color::rgb(0.9, 0.9, 0.9),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(100.0, 80.0, 0.0),
+            ..Default::default()
+        })
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(15.0, 50.0, 0.1),
+            border_radius: None,
+        });
+    // bottom
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(500.0, 10.0)),
+                color: Color::rgb(0.9, 0.9, 0.9),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(100.0, -300.0, 0.0),
+            ..Default::default()
+        })
+        .insert(RigidBody::Static)
+        .insert(CollisionShape::Cuboid {
+            half_extends: Vec3::new(250.0, 5.0, 0.1),
+            border_radius: None,
+        });
+}
