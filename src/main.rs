19962c2d6c73fb0098628a4b9569a895b5dc1ce7
Ida Iyes <identry@protonmail.com>
2022-02-26 09:42:47 +0200

    load ui assets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_asset_loader::{AssetLoader, AssetCollection};
 
 /// Each level/map in the game
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -10,6 +11,7 @@ enum GameMode {
 /// Application states
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum AppState {
+    MainAssetLoading,
     MainMenu,
     Loading(GameMode),
     InGame(GameMode),
@@ -37,13 +39,31 @@ fn main() {
     app.insert_resource(ClearColor(Color::BLACK));
     app.add_plugins(DefaultPlugins);
 
+    // our state enum
+    app.add_state(AppState::MainAssetLoading);
+
     // 3rdparty plugins
     app.add_plugin(bevy_tweening::TweeningPlugin);
     app.add_plugin(benimator::AnimationPlugin::default());
     app.add_plugin(bevy_kira_audio::AudioPlugin);
 
-    // our state enum and game stuff
-    app.add_state(AppState::MainMenu);
+    // assets loader
+    AssetLoader::new(AppState::MainAssetLoading)
+        .continue_to_state(AppState::MainMenu)
+        .with_asset_collection_file("meta/ui.assets")
+        .with_collection::<UiAssets>()
+        .build(&mut app);
+
+    // our game stuff
+    //app.add_plugin(ui::mainmenu::MainMenuPlugin(AppState::MainMenu));
 
     app.run();
+}
+
+#[derive(AssetCollection)]
+struct UiAssets {
+    #[asset(key = "ui.font_menu_bold")]
+    font_menu_bold: Handle<Font>,
+    #[asset(key = "ui.font_menu_regular")]
+    font_menu_regular: Handle<Font>,
 }
